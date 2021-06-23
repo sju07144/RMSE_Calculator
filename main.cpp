410d143bc24ff1682425a1b0f8df703ec8206914
영서 김 <sju07144@naver.com>
2021-06-23 20:21:26 +0900

    modification 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2676579a4661b887307ce12585982b93564c46ddbd1fc61bfbbb57cc0f7a04bb
-size 11202
+oid sha256:6dcd46143c9141aa313e85a9bde0a3964d1f04e96dfd45232500eed4d90500d4
+size 104
